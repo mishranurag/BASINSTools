5276fe9cf0df00830ca4b58a57f28723b5c30f83
Anurag Mishra <mishranurag@gmail.com>
2023-02-23 11:33:47 -0800

    FTABLE Calculation Scripts

--- WDMOutputToDataFrame.py
+++ WDMOutputToDataFrame.py
@@ -4,11 +4,13 @@ It can easily be modified to loop through all the datasets.'''
 
 from wdmtoolbox import wdmtoolbox as wdm
 import pandas as pd
-files_path='C:\BASINS45\modelout\Test'
-wdmfile= os.path.join(files_path,'Test.wdm')
+import os
+
+files_path=r'C:\BASINS45\modelout\HSPF_Test'
+wdmfile= os.path.join(files_path,'HSPF_Test.wdm')
 DataList=wdm.listdsns(wdmfile) #Getting the list of all datasets from the WDMFile as OrderedDict
 DSNList=list(DataList.keys()) #Extracting the list of DSN from DataList
 dataSeries=wdm.extract(wdmfile,DSNList[0]) #extracting the dataset using the first DSN in DSNList
-dataSeries=dataSeries['2002-05-31':'2002-07-31'] #subsetting the data
+#dataSeries=dataSeries['2002-05-31':'2002-07-31'] #subsetting the data
 dataSeries.plot() #plotting the dataframe
 #dataSeries is a dataframe
